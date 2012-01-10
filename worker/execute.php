34f979592714bf6a2b766b946f4824978246f543
Paddy Foran <paddy@secondbit.org>
2012-01-10 00:22:05 -0500

    Updated to fix use of json_decode results as array.

--- worker/execute.php
+++ worker/execute.php
@@ -3,8 +3,9 @@
 include("lib/IronMQ/IronMQ.class.php");
 
 function getTweet($query) { 
-    $results = file_get_contents("http://search.twitter.com/search.json?q=".$query."&rpp=1");
-    return $results["results"][0];
+    $res = file_get_contents("http://search.twitter.com/search.json?q=".$query."&rpp=1");
+    $results = json_decode($res);
+    return $results->results[0];
 }
 
 $config = parse_ini_file('config.ini', true);
@@ -15,7 +16,7 @@ print_r($tweet);
 
 $mq = new IronMQ('config.ini', true);
 
-$response = $mq->postMessage('', "TweetWorker", array("body" => $tweet["text"]));
+$response = $mq->postMessage('', "TweetWorker", array("body" => $tweet->text));
 
 print_r($response);
 
