4b7401296485de15d40599903da2756cdc043f09
Paddy Foran <foran.paddy@gmail.com>
2012-01-09 19:42:36 -0500

    first commit.

---
+++ tweetgrabber.php
@@ -0,0 +1,10 @@
+<?php
+
+function getTweet($query) { 
+    return file_get_contents("http://search.twitter.com/search.json?q=".$query."&rpp=1");
+}
+
+$config = parse_ini_file('config.ini', true);
+print_r(getTweet($config['query']));
+
+?>
